11682f93341cb4f28d37f140d959248402e06c72
hasanul banna <bannawandoor@gmail.com>
2023-02-23 17:36:36 +0530

    row post css added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 import './Banner.css'
-import {API_KEY}   from '../../constants/constatnts'
+import {API_KEY,imageBaseUrl}   from '../../constants/constatnts'
 import axios from '../../axios'
 function Banner() {
+  const [movie,setMovie] = useState()
+
   useEffect(()=>{
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-      console.log(response.data)
+
+      setMovie(response.data.results[Math.floor(Math.random() * response.data.results.length)])
+
       
     })
   },[])
 
   return (
-    <div className='banner'>
+    <div className='banner' style={{backgroundImage:`url(${ movie ?imageBaseUrl+ movie.backdrop_path : ''})`}}>
         <div className='content'>
-            <h1 className='title'> Movie Name</h1>
+            <h1 className='title'> { movie?movie.title||movie.original_name:''}</h1>
             <div className="banner_buttons">
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. 
+            <h1 className='description'>
+              {movie?movie.overview:''}
+              
             </h1>
         </div>
         <div className="fade_bottom"></div>
--- src/components/RowPost/RowPost.jsx
+++ src/components/RowPost/RowPost.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import axios from '../../axios'
+import './RowPost.css'
+import { API_KEY,imageBaseUrl} from '../../constants/constatnts'
 
 function RowPost() {
+  const [movies,setMovies] = useState([])
+  
+  useEffect(() =>{
+    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response=>{
+      console.log(response.data.results)
+      setMovies(response.data.results)
+
+    })
+    
+  })
   return (
-    <div>
-        
+    <div className='row'>
+        <h2 >Netflix Originals</h2>
+        <div className="posters">
+          {movies.map(obj=>{
+            <img src={`${imageBaseUrl+obj.backdrop_path}`} alt="poster" className='poster' />
+          })}
+        </div>
     </div>
   )
 }
